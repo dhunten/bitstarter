485b34b86b3aaa3dd521460291e83524f4847fba
dhunten <dhunten@gmail.com>
2013-07-12 04:47:18 +0000

    read fomr index.html

--- web.js
+++ web.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var index = fs.readFileSync('index.html', 'utf-8');
+
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
 });
