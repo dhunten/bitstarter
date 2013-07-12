3a33a3830735c4be284ecc0d180b26ec3cb8b950
dhunten <dhunten@gmail.com>
2013-07-12 05:03:22 +0000

    adads

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 var index = fs.readFileSync('index.html', 'utf-8');
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(index);
 });
 
 var port = process.env.PORT || 5000;
